4a6a1aab351a6a47f547efd4daebf4f280ea276a
kubi <kubilayyildizz@gmail.com>
2023-11-30 11:10:26 +0100

    My First Flask Project Has Been Updated

---
+++ project.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def head():
+    return 'Hello :)'
+
+@app.route('/second')
+def second():
+    return 'This is second page'
+
+@app.route('/third')
+def third():
+    return 'And this is 3rd page'
+
+@app.route('/fourth/<string:id>')
+def fourth(id):
+    return f'ID of this page is {id}'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=30000)
+    # app.run(host= '0.0.0.0', port=8081)
