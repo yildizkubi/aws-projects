6c36fab84d6e2137cc62bcdb196b4ec1cedefff3
kubi <kubilayyildizz@gmail.com>
2023-12-01 14:24:29 +0100

    commit

--- flask-02/jinja.py
+++ flask-02/jinja.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,6 +12,6 @@ def number():
     y=19.25
     return render_template('body.html', value1=x, value2=y, sum=x*y)
 
-if __name__ == '__main__':
-    app.run(debug=True, port=8081)
-    # app.run(host= '0.0.0.0', port=8081)
+if __name__== "__main__":
+    # app.run(debug=True, port=30000)
+    app.run(host= '0.0.0.0', port=8081)
